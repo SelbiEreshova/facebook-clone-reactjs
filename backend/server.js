91d15f02c80e71a87bb04f06170a2bc71a90d2bd
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-06-26 13:12:19 +0300

    Code style updated

--- backend/server.js
+++ backend/server.js
@@ -1,44 +1,26 @@
 const express = require("express");
-const cors = require("cors");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const { readdirSync } = require("fs");
 const dotenv = require("dotenv");
 dotenv.config();
 
-//Create the application
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-//Add cors to application
-const options = {
-  origin: "http://localhost:3000",
-  useSuccessStatus: 200,
-};
-app.use(cors(options));
+//routes
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-//Use json
-app.use(express.json);
-
-//Dynamically get all the routes. No need to update this when new route is added
-readdirSync("./routes").map((r) => {
-  app.use("/", require("./routes/" + r));
-});
-
-//Connect to database
+//database
 mongoose
   .connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
   })
-  .then(() => {
-    console.log("Database connected successfully");
-  })
-  .catch((err) => {
-    console.log("Error connecting to Mongo db", err);
-  });
+  .then(() => console.log("database connected successfully"))
+  .catch((err) => console.log("error connecting to mongodb", err));
 
-//Get port
 const PORT = process.env.PORT || 8000;
-
-//Listen for connections
 app.listen(PORT, () => {
-  console.log(`server is running on ${PORT}`);
+  console.log(`server is running on port ${PORT}..`);
 });
