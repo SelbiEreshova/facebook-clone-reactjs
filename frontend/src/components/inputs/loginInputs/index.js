4254814b0cb7573841235e77c4ae6caee8435b16
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-07-11 14:03:17 +0300

    Added login page

--- frontend/src/components/inputs/loginInputs/index.js
+++ frontend/src/components/inputs/loginInputs/index.js
@@ -1,16 +1,68 @@
 import "./style.css";
-import { useField } from "formik";
-export default function LoginInput({ placeholder, ...props }) {
+import { ErrorMessage, useField } from "formik";
+import { useMediaQuery } from "react-responsive";
+export default function LoginInput({ placeholder, bottom, ...props }) {
   const [field, meta] = useField(props);
+  const desktopView = useMediaQuery({
+    query: "(min-width: 850px)",
+  });
   return (
     <div className="input_wrap">
+      {meta.touched && meta.error && !bottom && (
+        <div
+          className={
+            desktopView ? "input_error input_error_desktop" : "input_error"
+          }
+          style={{ transform: "translateY(3px)" }}
+        >
+          {
+            //We want to show the error only when the user at leat clicked
+            //the input are, not when the page loaded
+            meta.touched && meta.error && <ErrorMessage name={field.name} />
+          }
+          {meta.touched && meta.error && (
+            <div
+              className={desktopView ? "error_arrow_left" : "error_arrow_top"}
+            ></div>
+          )}
+        </div>
+      )}
+
       <input
+        className={meta.touched && meta.error ? "input_error_border" : ""}
         type={field.type}
         placeholder={placeholder}
         name={field.name}
         {...field}
         {...props}
       ></input>
+      {meta.touched && meta.error && bottom && (
+        <div
+          className={
+            desktopView ? "input_error input_error_desktop" : "input_error"
+          }
+          style={{ transform: "translateY(1 px)" }}
+        >
+          {
+            //We want to show the error only when the user at leat clicked
+            //the input are, not when the page loaded
+            meta.touched && meta.error && <ErrorMessage name={field.name} />
+          }
+          {meta.touched && meta.error && (
+            <div
+              className={
+                desktopView ? "error_arrow_left" : "error_arrow_bottom"
+              }
+            ></div>
+          )}
+        </div>
+      )}
+      {meta.touched && meta.error && (
+        <i
+          className="error_icon"
+          style={{ top: `${!bottom && !desktopView ? "63%" : "15px"}` }}
+        ></i>
+      )}
     </div>
   );
 }
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -3,6 +3,7 @@ import { Formik, Form } from "formik";
 import { Link } from "react-router-dom";
 import LoginInput from "../../components/inputs/loginInputs";
 import { useState } from "react";
+import * as Yup from "yup";
 const loginInfos = {
   email: "",
   password: "",
@@ -17,6 +18,13 @@ export default function Login() {
     setLogin({ ...login, [name]: value });
     console.log(value);
   };
+  const loginValidation = Yup.object({
+    email: Yup.string()
+      .required(`Email address  is required`)
+      .email(`Must be a valid email`)
+      .max(100),
+    password: Yup.string().required("Passwrod is required"),
+  });
   return (
     <div className="login">
       <div className="login_wrapper">
@@ -35,6 +43,7 @@ export default function Login() {
                   email,
                   password,
                 }}
+                validationSchema={loginValidation}
               >
                 {(formik) => (
                   <Form>
@@ -43,12 +52,14 @@ export default function Login() {
                       name="email"
                       placeholder="Email address or phone number"
                       onChange={handleLoginOnChange}
+                      bottom={false}
                     />
                     <LoginInput
                       type="password"
                       name="password"
                       placeholder="Password"
                       onChange={handleLoginOnChange}
+                      bottom={true}
                     />
                     <button type="submit" name="password " className="blue_btn">
                       Log In
@@ -63,12 +74,68 @@ export default function Login() {
               <button className="blue_btn open_sign_up">Create Account</button>
             </div>
             <Link to="" className="sign_extra ">
-              <b>Create a Page</b>
-              for a celebrity, brand or business
+              <b>Create a Page</b> for a celebrity, brand or business
             </Link>
           </div>
         </div>
         <div className="register"></div>
+        <footer className="login_footer">
+          <div className="login_footer_wrap">
+            <Link to="/">English(UK)</Link>
+            <Link to="/">Français(FR)</Link>
+            <Link to="/">العربية</Link>
+            <Link to="/">ⵜⴰⵎⴰⵣⵉⵖⵜ</Link>
+            <Link to="/">Español (España)</Link>
+            <Link to="/">italiano</Link>
+            <Link to="/">Deutsch</Link>
+            <Link to="/">Português (Brasil)</Link>
+            <Link to="/">हिन्दी</Link>
+            <Link to="/">中文(简体)</Link>
+            <Link to="/">日本語</Link>
+            <Link to="/" className="footer_square">
+              <i className="plus_icon"></i>
+            </Link>
+          </div>
+          <div className="footer_splitter"></div>
+          <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+            <Link to="/">Log in</Link>
+            <Link to="/">Messenger</Link>
+            <Link to="/">Facebook Lite</Link>
+            <Link to="/">Watch</Link>
+            <Link to="/">Places</Link>
+            <Link to="/">Games</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Facebook Pay</Link>
+            <Link to="/">Oculus</Link>
+            <Link to="/">Portal</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Bulletin</Link>
+            <Link to="/">Local</Link>
+            <Link to="/">Fundraisers</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Voting Information Centre</Link>
+            <Link to="/">Groups</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Create ad</Link>
+            <Link to="/">Create Page</Link>
+            <Link to="/">Developers</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Privacy</Link>
+            <Link to="/">Cookies</Link>
+            <Link to="/">
+              AdChoices
+              <i className="adChoices_icon"></i>
+            </Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Help</Link>
+          </div>
+          <div className="login_footer_wrap">
+            <Link to="/" style={{ fontSize: "12px", marginTop: "10px" }}>
+              Meta © 2022
+            </Link>
+          </div>
+        </footer>
       </div>
     </div>
   );
