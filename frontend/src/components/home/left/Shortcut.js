176322d4756db1dc1a0a40d0bb67ee0e325334f6
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-07-20 22:47:40 +0300

    Home page left bar finished

---
+++ frontend/src/components/home/left/Shortcut.js
@@ -0,0 +1,9 @@
+export default function Shortcut({ link, img, name }) {
+    return (
+      <a href={link} target="_blank" rel="noreferrer" className="shortcut_item">
+        <img src={img} alt="" />
+        <span>{name}</span>
+      </a>
+    );
+  }
+  
--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,9 +1,13 @@
 import Header from "../../components/headers";
-
+import LeftHome from "../../components/home/left";
+import { useSelector } from "react-redux";
 export default function Home() {
+  const { user } = useSelector((user) => ({ ...user }));
+
   return (
     <div>
       <Header />
+      <LeftHome user={user} />
     </div>
   );
 }
