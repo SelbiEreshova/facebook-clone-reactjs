97f4e94777e7eb92a63358e8e41f7d09f613e08e
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-07-24 21:52:14 +0300

    Resend verification added

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -5,8 +5,9 @@ import RightHome from "../../components/home/right";
 import Stories from "../../components/home/stories";
 import CreatePost from "../../components/createPost";
 import  './style.css'
+import SendVerification from "../../components/sendVerification";
 export default function Home() {
-  const { user } = useSelector((user) => ({ ...user }));
+  const { user } = useSelector((state) => ({ ...state }));
 
   return (
     <div className="home">
@@ -14,6 +15,7 @@ export default function Home() {
       <LeftHome user={user} />
       <div className="home_middle">
         <Stories/>
+        {user.verified === false && <SendVerification user={user} />}
           <CreatePost user={user}/>
       </div>
       <RightHome user={user}/>
